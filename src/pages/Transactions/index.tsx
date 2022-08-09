6eb5b3ac8254aac3dec5b1f1b2d7f9d95064f17f
Jefferson <jeferson.oliveira@g.globo>
2022-08-09 19:24:00 -0300

    criando tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import Header from "../../components/Header";
 import Summary from "../../components/Summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 const Transactions = () => {
   return (
     <>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 10.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Ifood</td>
+              <td>
+                <PriceHighLight variant="outcome">R$ 5.000,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>20/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   );
 };
