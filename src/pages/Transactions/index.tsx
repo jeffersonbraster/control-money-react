d0ed925df295ff7bfb64471580b1a244871419b0
Jefferson <jeferson.oliveira@g.globo>
2022-08-10 20:28:02 -0300

    criando o contexto de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Summary from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import SearchForm from "./components/SearchForm";
 import {
   PriceHighLight,
@@ -8,29 +9,8 @@ import {
   TransactionsTable,
 } from "./styles";
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: "income" | "outcome";
-  value: number;
-  price: number;
-  category: string;
-  createdAt: Date;
-}
-
 const Transactions = () => {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  const loadTransactions = async () => {
-    const response = await fetch("http://localhost:3000/transactions");
-    const data = await response.json();
-
-    setTransactions(data);
-  };
-
-  useEffect(() => {
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <>
